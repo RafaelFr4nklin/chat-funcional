b223e776a013babf89368022c863a09839d8940b
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-14 20:49:35 -0300

    Primeiros Passos

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,8 +1,17 @@
 const {WebSocketServer} = require("ws");
 const dotenv = requere("dotenv");
 
+dotenv.config();
+
 const wss = new WebSocketServer({port:process.env.PORT || 8080});
 
-wss.on("connection", (WebSocketServer) =>{
+wss.on("connection", (ws) =>{
+    ws.on("error", console.error);
+
     
+    ws.on("menssage", (data) => {
+        wss.clients.forEach((clients) => clients.send(data.toString()));
+    });
+
+    console.log("client conect");
 });
