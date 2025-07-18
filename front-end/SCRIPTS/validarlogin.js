1e4c0afdc7fca82261d68ac5d3ba3cde49de33a5
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-18 20:49:26 -0300

    As mensagens do usuário estão funcionando

--- front-end/SCRIPTS/validarlogin.js
+++ front-end/SCRIPTS/validarlogin.js
@@ -5,8 +5,20 @@ const form_input = login.querySelector(".login_input");
 const chat = document.getElementById("chat");
 const chatFormu = chat.querySelector(".chat_form");
 const chatinput = chat.querySelector(".chat-input");
+const chatmensagens = chat.querySelector("#chat-mensagens");
 
 let ws;
+
+
+function escrevaVisual(contet){
+    const div = document.createElement("div");
+
+    div.classList.add("mensUser");
+    div.innerHTML = contet;
+
+    return div
+}
+
 
 const cores = ['blueviolet', 'cadetblue', 'brown', 'coral', 'cornflowerblue'];
 
@@ -21,9 +33,21 @@ const Usuario = {
     corUser: ""
 };
 
-function mensagemEnvia(event) {
-    console.log("Mensagem do servidor:", event.data);
+function mensagemEnvia({ data }) {
+
+    const{usuarioID, usuarioNome, usuarioCor, contet} = JSON.parse(data);
+
+    const elemento = escrevaVisual(contet);
+
+    chatmensagens.appendChild(elemento);
+    // try {
+    //     const json = JSON.parse(data);
+    //     console.log("Mensagem recebida (JSON):", json);
+    // } catch (err) {
+    //     console.warn("Mensagem recebida (texto simples):", data);
+    // }
 }
+
 
 const submitFunc = (e) => {
     e.preventDefault();
@@ -48,10 +72,20 @@ const submitFunc = (e) => {
 function escreveMens(e) {
     e.preventDefault();
 
+    const mensagem = {
+        usuarioID:Usuario.id,
+        usuarioNome:Usuario.nome,
+        usuarioCor:Usuario.corUser,
+        contet: chatinput.value
+
+    }
+
     const texto = chatinput.value.trim();
     if (!texto || ws.readyState !== 1) return;
 
-    ws.send(`${Usuario.nome}: ${texto}`);
+    ws.send(JSON.stringify(mensagem));
+
+
     chatinput.value = "";
 }
 
